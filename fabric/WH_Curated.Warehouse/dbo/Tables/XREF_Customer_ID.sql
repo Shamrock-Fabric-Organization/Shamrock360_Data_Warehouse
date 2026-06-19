2f1fa0fbf06d2838febddc50c1a343b1e4c11cd9
Keith Chafin <126492438+keithchafin@users.noreply.github.com>
2026-06-19 09:03:41 -0400

    chore: remove warehouse DDL from Git tracking

--- fabric/WH_Curated.Warehouse/dbo/Tables/XREF_Customer_ID.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE [dbo].[XREF_Customer_ID] (
-
-	[Company] varchar(50) NULL, 
-	[Apollo_CustomerID] varchar(50) NULL, 
-	[D365_CustomerID] varchar(50) NULL
-);
--- fabric/WH_Curated.Warehouse/dbo/Tables/tbl_DIM_AgingBuckets.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE [dbo].[tbl_DIM_AgingBuckets] (
-
-	[AgingBucketKey] int NULL, 
-	[FromDays] int NULL, 
-	[ToDays] int NULL, 
-	[Description] varchar(10) NULL
-);

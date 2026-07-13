15be7cb4a2f95ed3c20d9f0dcb73c165ad4e24ec
KeithChafin <126492438+keithchafin@users.noreply.github.com>
2026-07-13 10:54:37 -0400

    Addition of ProductOrderVariance fact and Cost Level Dimension Also, update for fact_SupplyChain_RAF to tie to actual data.

---
+++ database_projects/WH_Curated/dbo/Tables/mtbl_EDW_Dim_ReportCurrency.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE dbo.mtbl_EDW_Dim_ReportCurrency(
+	[Report_Currency_Code] [varchar](11) NOT NULL,
+	[Report_Currency_Name] [varchar](21) NOT NULL,
+	[Sort_Order] [int] NOT NULL
+)
+GO
---
+++ database_projects/WH_Curated/dbo/Tables/tbl_Dim_ProductionVarianceCostLevel.sql
@@ -0,0 +1,10 @@
+
+
+CREATE TABLE dbo.tbl_Dim_ProductionVarianceCostLevel(
+	[Cost_Level] [varchar](6) NOT NULL,
+	[Cost_Level_Key] [int] NOT NULL,
+	[Is_Default] [int] NOT NULL,
+	[Sort_Order] [int] NOT NULL,
+	[Cost_Level_Description] [varchar](110) NOT NULL
+)
+GO
